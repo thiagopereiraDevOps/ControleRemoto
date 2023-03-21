490480f70502bd45699ff96132481fefd35a6e5b
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-03-21 13:59:01 -0300

    Meus arquivos de ControleRemoto

---
+++ src/Exemplo1.java
@@ -0,0 +1,10 @@
+public class Exemplo1 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.maisVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
